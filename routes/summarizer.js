db8b7afd8d7d0eff9c3feb8b6b1bfcb0e9b47452
ParthivmMS <parthivmssince2005@gmail.com>
2025-08-29 10:38:50 +0530

    Delete routes directory

--- routes/summarizer.js
+++
@@ -1,9 +0,0 @@
-// summarizer.js
-
-async function summarize(text) {
-  // Dummy summarizer: return first 3 sentences
-  const sentences = text.split(".");
-  return sentences.slice(0, 3).join(".") + ".";
-}
-
-module.exports = summarize;
